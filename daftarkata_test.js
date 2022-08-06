4b622fe80e39bb53a1402baca113fb824baeea25
Muhammad Refa Utama Putra <m.r3f4.u.p@gmail.com>
2022-08-06 19:13:16 +0700

    Initial commit

---
+++ daftarkata_test.js
@@ -0,0 +1,17 @@
+// Capitalized = true
+var words = [
+    ["A", "a"],
+    ["B", "b"],
+    ["C", "c"],
+    ["D", "d"],
+    ["E", "e"],
+    ["F", "f"],
+    ["I", "i"],
+    ["J", "j"],
+    ["K", "k"],
+    ["L", "l"],
+    ["M", "m"],
+    ["N", "n"],
+];
+
+var words_count = words.length;
